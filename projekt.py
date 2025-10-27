8ddaac3674505809faf270594584dec70cf591d4
Mehraz <mehraz.patwary@elev.ga.ntig.se>
2025-10-27 15:08:29 +0100

    Superklass

--- projekt.py
+++ projekt.py
@@ -1,1 +1,11 @@
-print("Hello world")
+import random 
+
+class karaktar: 
+    def __init__(self, namn, halsa, attackkraft):
+        self.namn = namn 
+        self.halsa = halsa
+        self.max_halsa = halsa
+        self.attackkraft = attackkraft
+        self.cooldown = 0
+
+        
